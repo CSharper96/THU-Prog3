9d889242ec12b1ab20b062d6a972ccf54ba6ff38
changed123 <j.waldenmayer@gmx.net>
2020-10-12 15:36:53 +0200

    Meine Version der Aufgaben hinzugefügt

---
+++ Übungen/Übung 1/C++/Aufgaben_Josua/Aufgabe01/team_members.h
@@ -0,0 +1,10 @@
+#ifndef _TEAM_MEMBERS_H
+#define _TEAM_MEMBERS_H
+
+void print_info_josua_waldenmayer();
+void print_info_otto_altfatter();
+void print_info_alex_merk();
+void print_info_thomas_horny();
+void print_info_anna_wilusch();
+
+#endif
