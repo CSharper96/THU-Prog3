9d9f542ce352759f9c16889a10da592dcba40830
Thomas <thomashorny@gmx.net>
2020-10-12 10:41:45 +0200

    Unnötige Ordner und Dateien entfernt

--- Übungen/Übung 1/C++/Neuer Ordner/Build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Studium Inf WS20_21/Prog3/Übungen/Übung 1/C++/Neuer Ordner
-# Build directory: D:/Studium Inf WS20_21/Prog3/Übungen/Übung 1/C++/Neuer Ordner/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
